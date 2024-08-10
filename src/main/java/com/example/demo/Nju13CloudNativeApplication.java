dc7e298882e9cfa0e3525cbc320af0afa5d63cdf
好闻天下 <horacezhu@outlook.com>
2024-08-10 10:38:27 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/Nju13CloudNativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nju13CloudNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nju13CloudNativeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Map<String, String> sayHello() {
+        Map<String, String> response = new HashMap<>();
+        response.put("msg", "hello");
+        return response;
+    }
+}
