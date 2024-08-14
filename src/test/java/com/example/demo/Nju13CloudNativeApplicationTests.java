ebe36d22e5b91d5d7f9eb561e6e8fd39d8c39905
wengtiancheng <14786471+wengtiancheng@user.noreply.gitee.com>
2024-08-14 11:14:45 +0800

    feat:unit test

--- src/test/java/com/example/demo/Nju13CloudNativeApplicationTests.java
+++ src/test/java/com/example/demo/Nju13CloudNativeApplicationTests.java
@@ -3,11 +3,19 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class Nju13CloudNativeApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void exampleTest() {
+		int expected = 42;
+		int actual = 40 + 2;
+		assertEquals(expected, actual, "The sum should be 42");
 	}
 
 }
